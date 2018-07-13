dbd81979532bc9e2e8bf54ee4f8cb3a1878cfdd9
Kemal Emre Ballı <33103502+kemaleb@users.noreply.github.com>
2018-07-13 17:33:13 +0300

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -14,8 +14,9 @@ public class Fibonacci {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args)
+        {
+        // TODO code application logic here // Bu kodumuz döngülerle yazılmış fibonacci serisidir.
         int sayi1=1;
         int sayi2=1;//1,1,2,3,5,8,13,21.....
         int gecici;
@@ -27,7 +28,24 @@ public class Fibonacci {
             sayi2=sayi1+sayi2;//sayi2 1+1 oldu
             sayi1=gecici;//sayi1 ise 1 sayısını geri aldı
             
+            
+       /*     Bu kodumuzda is dizilerle fibonacci çözümünü gösterdik.
+        int fib[] =new int[10];//dizinin içini düzenlemeyi ayrı bir döngüde, 
+        fib[0]=1;              //yazdırmayı ayrı bir döngüde yapmakta fayda var. 
+        fib[1]=1;
+        
+        for (int i = 0; i < 8; i++)
+        {
+            fib[i+2]=fib[i+1]+fib[i];
         }
+        for (int i = 0; i < 10; i++) 
+        {
+            System.out.println(fib[i]);
+        }
+            
+            
+            
+        }*/
         
         
     }
