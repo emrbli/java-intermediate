f75cab5108f5c5cb20ab568bc73d7bc7524200c3
Kemal Emre Ballı <33103502+kemaleb@users.noreply.github.com>
2018-07-16 15:11:56 +0300

    Fonksiyon İle Faktöriyel

---
+++ Factoriyel.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package faktoriyel;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Kemal
+ */
+public class Faktoriyel {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static int factorial(int num)
+    {
+        int b =1;
+    
+    for(int i=2;i<=num;i++)
+      {
+        b=i*b;
+        
+      }
+      System.out.println(b);
+      return num;
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+   // girilen sayının faktöriyelini almak.
+    int a;
+    a =input.nextInt();
+   
+    factorial(a);
+      
+  }   
+  
+    }
+    
+
