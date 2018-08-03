e52dd9a76f0d3beb20219842ee529a1ea5061087
Kemal Emre Ballı <kemal.emre.balli@gmail.com>
2018-08-03 17:30:09 +0300

    JsonMetinBelgesi

---
+++ Person.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gsonexample;
+
+/**
+ *
+ * @author Kemal
+ */
+public class Person {
+    String name;
+    String surname;
+    String city;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "name=" + name + ", surname=" + surname + ", city=" + city + '}';
+    }
+   
+}
