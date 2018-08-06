5139d799b483e0e296a897f19aa9c3c3c4820888
Kemal Emre Ballı <kemal.emre.balli@gmail.com>
2018-08-06 17:25:35 +0300

    VeriGetSet

---
+++ BB_JsoupİleVeri.java
@@ -0,0 +1,63 @@
+//Broadcast.java(class)
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.broadcast;
+
+/**
+ *
+ * @author Kemal
+ */
+public class Broadcast {
+    private String time;
+    private String imageUrl;
+    private String title;
+    private String tag;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    void add(Broadcast broadcast) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+    
+    
+    
+    
+}
